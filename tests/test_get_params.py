35b96fd5f59b618264fdaaae6daa316786c1aedc
mm523 <martina.milighetti15@gmail.com>
2026-05-13 18:12:05 -0400

    add tests - update tests for test_noise_function to use var_btn_tps

---
+++ tests/test_get_params.py
@@ -0,0 +1,109 @@
+'''
+Test parameter extraction from noise function
+'''
+
+import pandas as pd
+import numpy as np
+from clonesearch import get_params as params
+
+class TestMakeDf:
+    '''
+    Test output
+    '''
+    def test_make_output_df(self):
+
+        N = {
+            ('time1','time3'): {
+                                ('75','50'):  30,
+                                ('100','75'): 50
+                            },
+        }
+        avgs = {
+            ('time1','time3'): {
+                                ('75','50'):  1,
+                                ('100','75'): 5
+                            },
+        }
+        dt = {
+            ('time1','time3'): {
+                                ('75','50'):  3,
+                                ('100','75'): 2
+                            },
+        }
+        f = {
+            ('time1','time3'): {
+                                ('75','50'):  4.5,
+                                ('100','75'): 5.5
+                            },
+        }
+
+
+        output_df = params._make_output_df(N, avgs, dt, f)
+
+        expected_output = pd.DataFrame(
+            columns = ['p1','p2','t1','t2', 'avgs', 'N', 'dt', 'freqs', 'percentiles'],
+            data = [
+                ['75', '50', 'time1', 'time3', 1, 30, 3, 4.5, '75-50'],
+                ['100', '75', 'time1', 'time3', 5, 50, 2, 5.5, '100-75']
+            ]
+        )
+
+        pd.testing.assert_frame_equal(output_df, expected_output)
+
+class TestFittingFunction:
+    '''
+    Test fits
+    '''
+    def test_prepare_fit_data(self):
+
+        input_df = pd.DataFrame(
+            columns = ['p1','p2','t1','t2', 'avgs', 'N', 'dt', 'freqs', 'percentiles'],
+            data = [
+                ['75', '50', 'time1', 'time3', 0, 30, 3, 4.5, '75-50'],
+                ['75', '50', 'time1', 'time3', 1, 30, 3, 4.5, '75-50'],
+                ['100', '75', 'time1', 'time3', 5, 50, 2, 5.5, '100-75'],
+                ['75', '50', 'time1', 'time3', 2, 30, 3, 4.5, '75-50'],
+                ['75', '50', 'time1', 'time3', 0, 30, 3, 4.5, '75-50'],
+            ]
+        )
+
+        expected_f = np.array([4.5,5.5,4.5])
+        expected_avg = np.array([1, 5, 2])
+
+        fit_f, fit_a = params.prepare_fit_data(input_df)
+
+        np.testing.assert_array_equal(expected_f, fit_f)
+        np.testing.assert_array_equal(expected_avg, fit_a)
+
+    def test_fitting(self):
+        x = np.linspace(1,100, 50)
+        b = 1.5
+        s = .8
+
+        y = b*x + (s*x)**2
+
+        fit_s, fit_b = params._get_fit(x, y)
+        np.testing.assert_almost_equal(fit_s, s)
+        np.testing.assert_almost_equal(fit_b, b)
+
+    def test_fitting2(self):
+        x = np.logspace(1e-6,1e-1, 500)
+        b = 1.5/1e4
+        s = .8
+
+        y = b*x + (s*x)**2
+
+        fit_s, fit_b = params._get_fit(x, y)
+        np.testing.assert_almost_equal(fit_s, s)
+        np.testing.assert_almost_equal(fit_b, b)
+
+    def test_fitting3(self):
+        x = np.logspace(1e-6,1e-1, 500)
+        b = 1.5/27562
+        s = 0.3 # this fails if >1 because of the set bounds
+
+        y = b*x + (s*x)**2
+
+        fit_s, fit_b = params._get_fit(x, y)
+        np.testing.assert_almost_equal(fit_s, s)
+        np.testing.assert_almost_equal(fit_b, b)
---
+++ tests/test_transform_and_norm.py
@@ -0,0 +1,68 @@
+'''
+Test that behaviour of g(f) is as expected
+'''
+
+import numpy as np
+from clonesearch import normalisation_functions as norm
+
+class TestNewTransform:
+    def test_get_g_of_f(self):
+
+        f = 0.1
+        Nr = 5000
+        beta = 5
+        sigma = 0.5
+        g_of_1 = 27.63501512
+
+        np.testing.assert_almost_equal(
+            norm.g_of_f(f, Nr, sigma, beta), 23.06526392 - g_of_1,
+            decimal = 5
+            )
+
+        f = 0.01
+        Nr = 12458
+        beta = 2.8
+        sigma = 0.3
+        g_of_1 = 52.60198
+        np.testing.assert_almost_equal(
+            norm.g_of_f(f, Nr, sigma, beta), 37.62914536 - g_of_1,
+            decimal = 5
+            )
+
+        f = 0
+        Nr = 12458
+        beta = 2.8
+        sigma = 0.3
+        g_of_1 = 52.60198
+        np.testing.assert_almost_equal(
+            norm.g_of_f(f, Nr, sigma, beta), 28.00166283 - g_of_1,
+            decimal = 5
+            )
+
+    def test_noise_by_size_norm(self):
+
+        Nr = np.array([12458, 7512, 74081])
+        beta = np.array([2.8, 2.8, 2.8])
+        sigma = 0.3
+
+        g_of_1_1 = 52.60198
+        g_of_1_2 = 49.23230504
+        g_of_1_3 = 64.48382867
+
+        f_array = np.array([
+            [0.1, 0, 2.4*1e-4],
+            [0, 2.4*1e-4, 0.1],
+            [2.4*1e-4, 0.1, 0],
+            [0.3, 0.056, 0.002]
+        ])
+
+        expected_output = np.array([
+            [44.9637747 - g_of_1_1, 26.3154587 - g_of_1_2, 38.5970617 - g_of_1_3],
+            [28.0016628 - g_of_1_1, 27.9051992 - g_of_1_2, 56.8148336 - g_of_1_3],
+            [30.0366299 - g_of_1_1, 41.6181069 - g_of_1_2, 33.9443165 - g_of_1_3],
+            [48.5984095 - g_of_1_1, 39.7373764 - g_of_1_2, 44.0930080 - g_of_1_3]
+        ])
+
+        X = norm.noise_by_size_norm(f_array, Nr, sigma, beta)
+
+        np.testing.assert_array_almost_equal(expected_output, X)
